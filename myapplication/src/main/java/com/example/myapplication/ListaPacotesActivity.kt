f39479c43e37b56d9723beddc83c6dd2c2dfb607
Felipe Paim Roos <felipepaimroos@gmail.com>
2024-04-17 20:48:35 -0300

    Criando o projeto

---
+++ myapplication/src/main/java/com/example/myapplication/ListaPacotesActivity.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ListaPacotesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_pacotes);
+
+
+
+
+    }
+}
