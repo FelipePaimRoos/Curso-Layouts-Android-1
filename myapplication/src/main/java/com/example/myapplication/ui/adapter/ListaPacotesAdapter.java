edf39ccdaf862e7045615ec89e060178095ca212
Felipe Paim Roos <felipepaimroos@gmail.com>
2024-05-06 20:28:08 -0300

    Realizando o processo de bind

--- myapplication/src/main/java/com/example/myapplication/ui/adapter/ListaPacotesAdapter.java
+++ myapplication/src/main/java/com/example/myapplication/ui/adapter/ListaPacotesAdapter.java
@@ -1,13 +1,20 @@
 package com.example.myapplication.ui.adapter;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.myapplication.R;
 import com.example.myapplication.model.Pacote;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
@@ -38,6 +45,23 @@ public class ListaPacotesAdapter extends BaseAdapter {
     @Override
     public View getView(int posicao, View convertView, ViewGroup parent) {
         View viewCriada = LayoutInflater.from(context).inflate(R.layout.item, parent, false);
+
+        Pacote pacote = pacotes.get(posicao);
+        TextView local = viewCriada.findViewById(R.id.local);
+        local.setText(pacote.getLocal());
+
+        ImageView imagem = viewCriada.findViewById(R.id.imagemCidade);
+        Resources resources = context.getResources();
+        int idDoDrawable = resources.getIdentifier(pacote.getImagem(), "drawable", context.getPackageName());
+        Drawable drawableImagemPacote = resources.getDrawable(idDoDrawable);
+        imagem.setImageDrawable(drawableImagemPacote);
+
+        TextView dias = viewCriada.findViewById(R.id.dias);
+        dias.setText(pacote.getDias() + " dias");
+
+        TextView preco = viewCriada.findViewById(R.id.preco);
+        preco.setText(pacote.getPreco().toString());
+
         return viewCriada;
     }
 }
