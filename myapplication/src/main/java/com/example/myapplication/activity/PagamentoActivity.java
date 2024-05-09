353a7a2bf346ca6c5d9c1f667a782d7082c2348c
Felipe Paim Roos <felipepaimroos@gmail.com>
2024-05-09 17:24:05 -0300

    Limitando Dígitos e realizando o bind

--- myapplication/src/main/java/com/example/myapplication/activity/PagamentoActivity.java
+++ myapplication/src/main/java/com/example/myapplication/activity/PagamentoActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication.activity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +10,31 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.myapplication.R;
+import com.example.myapplication.model.Pacote;
+import com.example.myapplication.util.MoedaUtil;
+
+import java.math.BigDecimal;
 
 public class PagamentoActivity extends AppCompatActivity {
+
+    public static final String TITULO_APP_BAR_PAGAMENTO = "Pagamento";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagamento);
+
+        setTitle(TITULO_APP_BAR_PAGAMENTO);
+
+        Pacote pacoteSaoPaulo = new Pacote("São Paulo", "@android:drawable/screen_background_dark", 2, new BigDecimal("243.99"));
+
+        mostraPreco(pacoteSaoPaulo);
+
+    }
+
+    private void mostraPreco(Pacote pacote) {
+        TextView preco = findViewById(R.id.pagamento_preco_do_pacote);
+        String moedaBrasileira = MoedaUtil.formataParaBrasileiro(pacote.getPreco());
+        preco.setText(moedaBrasileira);
     }
 }
