1a8d68b31d36f2f7ab57e65bdbc92523aaa8e927
Felipe Paim Roos <felipepaimroos@gmail.com>
2024-05-09 11:29:19 -0300

    Adicionando a data da Viagem e Título

--- myapplication/src/main/java/com/example/myapplication/activity/ResumoPacoteActivity.java
+++ myapplication/src/main/java/com/example/myapplication/activity/ResumoPacoteActivity.java
@@ -21,6 +21,8 @@ import com.example.myapplication.util.ResourceUtil;
 import org.w3c.dom.Text;
 
 import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class ResumoPacoteActivity extends AppCompatActivity {
 
@@ -29,20 +31,33 @@ public class ResumoPacoteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resumo_pacote);
 
+        setTitle("Resumo do Pacote");
+
         Pacote pacoteSaoPaulo = new Pacote("São Paulo", "@drawable/ic_launcher_background", 2, new BigDecimal("243.99"));
 
-        TextView local = findViewById(R.id.NomeCidadeResumo);
+        TextView local = findViewById(R.id.resumo_pacote_local);
         local.setText(pacoteSaoPaulo.getLocal());
-        ImageView imagem = findViewById(R.id.imagemCidadeResumo);
+        ImageView imagem = findViewById(R.id.resumo_imagem_cidade);
         Drawable drawableDoPacote = ResourceUtil.devolveDrawable(this, pacoteSaoPaulo.getImagem());
         imagem.setImageDrawable(drawableDoPacote);
 
-        TextView dias = findViewById(R.id.QuantidadeDiasResumo);
+        TextView dias = findViewById(R.id.resumo_pacote_dias);
         String diasEmTexto = DiasUtil.formataEmTexto(pacoteSaoPaulo.getDias());
         dias.setText(diasEmTexto);
 
-        TextView preco = findViewById(R.id.precoResumo);
+        TextView preco = findViewById(R.id.resumo_pacote_preco);
         String moedaBrasileira = MoedaUtil.formataParaBrasileiro(pacoteSaoPaulo.getPreco());
         preco.setText(moedaBrasileira);
+
+        TextView data = findViewById(R.id.resumo_pacote_data);
+        Calendar dataIda = Calendar.getInstance();
+        Calendar dataVolta = Calendar.getInstance();
+        dataVolta.add(Calendar.DATE, pacoteSaoPaulo.getDias());
+        SimpleDateFormat formatoBrasileiro = new SimpleDateFormat("dd/MM");
+        String dataFormatadaIda = formatoBrasileiro.format(dataIda.getTime());
+        String dataFormatadaVolta = formatoBrasileiro.format(dataVolta.getTime());
+        String dataFormatadaDaViagem = dataFormatadaIda + " - " + dataFormatadaVolta + " de " + dataVolta.get(Calendar.YEAR);
+        data.setText(dataFormatadaDaViagem);
+
     }
 }
